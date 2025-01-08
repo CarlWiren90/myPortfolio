f74c68333bdadb61e9392cd8ce5706eb000b215c
CarlWiren90 <156791432+CarlWiren90@users.noreply.github.com>
2025-01-08 14:21:56 +0100

    Fixed early animation and parallax effect

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 import './style.css'
+/* import { movingWords } from './sections/projects'
+ */
 
-
+/* document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('scroll', () => {
+        console.log('Scroll event detected!');
+        movingWords()
+    })
+}) */
---
+++ src/sections/projects.js
@@ -0,0 +1,8 @@
+'use strict'
+import './projects.css';
+
+
+/* export const movingWords = () => {
+    const words = document.querySelector('.rolling-word')
+    words.style.transform = 'translateX(50px)'
+} */
