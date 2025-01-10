49b2bf8398cb537c84f9a366e835b121d94eac47
CarlWiren90 <156791432+CarlWiren90@users.noreply.github.com>
2025-01-10 09:15:30 +0100

    Small animation update

--- src/main.js
+++ src/main.js
@@ -1,15 +1,7 @@
 import './style.css'
-/* import { movingWords } from './sections/projects'
- */
-
-/* document.addEventListener('DOMContentLoaded', () => {
-    document.addEventListener('scroll', () => {
-        console.log('Scroll event detected!');
-        movingWords()
-    })
-}) */
 
 const appWindow = document.getElementById('app');
 appWindow.addEventListener('scroll', () => {
     console.log('scrolling')
 })
+
