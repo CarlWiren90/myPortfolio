572fb7f10824f5387d87d0f60629cb756faa89b5
CarlWiren90 <156791432+CarlWiren90@users.noreply.github.com>
2025-01-13 13:53:32 +0100

    Fixed scrolling animation

--- src/main.js
+++ src/main.js
@@ -1,16 +1,11 @@
 import './style.css'
 import { rollingWordsAnimation } from './sections/animations/rollingProjectsAnimation.js';
 
-
 const appWindow = document.getElementById('app');
-
-
-appWindow.addEventListener('scroll', () => {
-/*     console.log('scrolling')
- */})
 
 document.addEventListener('DOMContentLoaded', () => {
     const rollingWordsElement = document.querySelector('.rolling-words-container')
+
     appWindow.addEventListener('scroll', () => {
         let topValue = rollingWordsElement.getBoundingClientRect().top;
         if (rollingWordsElement && topValue < 750) {
